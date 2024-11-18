0d3ce75acbb07f47e69d4423a1f8c444852a397a
garimasingh <garimasingh19092003@gmail.com>
2024-11-19 00:35:21 +0530

    First

--- server/mail/templates/contactFormRes.js
+++ server/mail/templates/contactFormRes.js
@@ -75,7 +75,7 @@ exports.contactUsEmail = (
   <body>
       <div class="container">
           <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-                  src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+                  src="https://i.ibb.co/F45JwGb/bandicam-2024-11-18-21-41-47-705-removebg-preview.png" alt="LearnEra"></a>
           <div class="message">Contact Form Confirmation</div>
           <div class="body">
               <p>Dear ${firstname} ${lastname},</p>
@@ -89,7 +89,7 @@ exports.contactUsEmail = (
               <p>We appreciate your interest and will get back to you shortly. </p>
           </div>
           <div class="support">If you have any further questions or need immediate assistance, please feel free to reach
-              out to us at <a href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+              out to us at <a href="mailto:info@LearnEra.com">info@LearnEra.com</a>. We are here to help!</div>
       </div>
   </body>
   
--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ Poojan © 2024 Studynotion</div>
+          <div className="text-center">Made with ❤️ LearnEra Team - Major Project</div>
         </div>
       </div>
     </div>
